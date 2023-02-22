b7890b6a0898cddcb543fe77236e7d3e845484e9
Lijegimenez <125736143+Lijegimenez@users.noreply.github.com>
2023-02-22 08:16:58 -0300

    Add files via upload

---
+++ java script/eventos.js
@@ -0,0 +1,63 @@
+function original1() {
+    Element =document.getElementById("pers");
+    Element.src="img/personal2.png"
+    }
+    function personal1() {
+        Element =document.getElementById("pers");
+        Element.src="img/personal.png";
+    }
+    
+    function original2() {
+        Element =document.getElementById("labo");
+        Element.src="img/portafolio2 (1).png";
+    }
+     function personal2() {
+        Element =document.getElementById("labo");
+        Element.src="img/portafolio (1).png";
+    }
+    
+    function original3() {
+        Element =document.getElementById("habi");
+        Element.src="img/habilidades2.png";
+    }
+    function personal3() {
+        Element =document.getElementById("habi");
+        Element.src="img/habilidades.png";
+    }
+    
+    function original4() {
+        Element =document.getElementById("estu");
+        Element.src="img/aprendi2.png";
+    }
+    function personal4() {
+        Element =document.getElementById("estu");
+        Element.src="img/aprendi.png";
+    }
+    
+    function original5() {
+        Element =document.getElementById("be");
+        Element.src="img/behance2.png";
+    }
+    function personal5() {
+        Element =document.getElementById("be");
+        Element.src="img/behance.png";
+    }
+    
+    function original6() {
+        Element =document.getElementById("gi");
+        Element.src="img/gmail2.png";
+    }
+    function personal6() {
+        Element =document.getElementById("gi");
+        Element.src="img/gmail.png";
+    }
+    
+    function original7() {
+        Element =document.getElementById("li");
+        Element.src="img/linkedin2.png";
+    }
+    function personal7() {
+        Element =document.getElementById("li");
+        Element.src="img/linkedin.png";
+    }
+    
